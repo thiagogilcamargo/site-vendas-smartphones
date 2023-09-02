91b2941e103b458f1ab432218e14591eb26e02ef
thiagogilcamargo <thiagogilcmargo@gmail.com>
2023-09-01 23:00:30 -0300

    Página de visualização de aparelho

---
+++ site-vendas-smartphones/src/components/VisualizarAparelho.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function VisualizarAparelho() {
+  return (
+    <div>
+      <h2>Detalhes do Aparelho</h2>
+      {/* Exibir detalhes do aparelho selecionado */}
+    </div>
+  );
+}
+
+export default VisualizarAparelho;
