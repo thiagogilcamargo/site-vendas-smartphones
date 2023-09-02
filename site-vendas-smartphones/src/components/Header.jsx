5595f25a074620637e30a786c0e6e73543efcb4e
thiagogilcamargo <thiagogilcmargo@gmail.com>
2023-09-01 22:29:54 -0300

    Componente de cabeçalho criado

---
+++ site-vendas-smartphones/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Site de Vendas de Smartphones e Tablets</h1>
+    </header>
+  );
+}
+
+export default Header;
