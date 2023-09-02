be5c70658c99ea47a889ce58497661027b4879c7
thiagogilcamargo <thiagogilcmargo@gmail.com>
2023-09-01 23:02:32 -0300

    Página de erro 404

---
+++ site-vendas-smartphones/src/components/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <div>
+      <h2>Erro 404</h2>
+      <p>A página que você está procurando não foi encontrada.</p>
+    </div>
+  );
+}
+
+export default NotFound;
