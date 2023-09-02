4ff2fd04b5c158e85245ae3503a2729c34e037d7
thiagogilcamargo <thiagogilcmargo@gmail.com>
2023-09-01 23:45:22 -0300

    npm install react-router-dom

--- site-vendas-smartphones/src/data/Aparelhos.js
+++ site-vendas-smartphones/src/data/Aparelhos.js
@@ -2,10 +2,11 @@ const aparelhos = [
   {
     id: 1,
     nome: 'Smartphone 1',
-    descricao: 'Descrição do Smartphone 1.',
-    // Outros dados do aparelho
+    descricao: 'Descrição do Smartphone 1...',
+    imagem: 'imagem1.jpg',
+    // Outros campos
   },
-  // Adicione mais aparelhos
+  // Adicione os outros aparelhos
 ];
 
 export default aparelhos;
