f04de21448fc0afbe491b02c574d80f0a4b6a916
thiagogilcamargo <thiagogilcmargo@gmail.com>
2023-09-01 22:59:35 -0300

    Página de listagem de aparelhos

---
+++ site-vendas-smartphones/src/components/Aparelhos.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Aparelhos() {
+  return (
+    <div>
+      <h2>Aparelhos</h2>
+      {/* Listagem de aparelhos a ser preenchida */}
+    </div>
+  );
+}
+
+export default Aparelhos;
