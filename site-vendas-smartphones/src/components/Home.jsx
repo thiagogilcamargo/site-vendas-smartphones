10b309b3da56b24dfb43fdef2e0b8cb541c8f0aa
thiagogilcamargo <thiagogilcmargo@gmail.com>
2023-09-01 22:47:54 -0300

    Página inicial com duas promoções fictícias

---
+++ site-vendas-smartphones/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h2>Promoções</h2>
+      <p>Promoção 1: Descrição da promoção 1.</p>
+      <p>Promoção 2: Descrição da promoção 2.</p>
+    </div>
+  );
+}
+
+export default Home;
+
