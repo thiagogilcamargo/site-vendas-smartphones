e7a981ba07047dc48f8737f964e21cba6cad0fe8
thiagogilcamargo <thiagogilcmargo@gmail.com>
2023-09-01 22:57:53 -0300

    Arquivo de dados simulados

---
+++ site-vendas-smartphones/src/data/Aparelhos.js
@@ -0,0 +1,12 @@
+const aparelhos = [
+  {
+    id: 1,
+    nome: 'Smartphone 1',
+    descricao: 'Descrição do Smartphone 1...',
+    imagem: 'imagem1.jpg',
+    // Outros campos
+  },
+  // Adicione os outros aparelhos
+];
+
+export default aparelhos;
